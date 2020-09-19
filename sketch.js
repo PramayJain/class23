817f2a73d7bd6e5ed8c0c748678561f18779959a
PramayJain <71068114+PramayJain@users.noreply.github.com>
2020-09-19 14:41:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+    box2= new Box(244,100,50,50);
+    box1 = new Box(200,300,50,50);
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    box2.display();
+    box1.display();
+}
